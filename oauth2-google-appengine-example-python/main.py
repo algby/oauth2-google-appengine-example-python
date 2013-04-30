d9196f9bc1ec4995bbd61562a6bbc42b510fcd57
spm2ot <sprunck.markus@gmail.com>
2013-04-30 19:45:31 +0200

    New client secrets file

--- oauth2-google-appengine-example-python/main.py
+++ oauth2-google-appengine-example-python/main.py
@@ -12,7 +12,7 @@ from google.appengine.ext.webapp import template
 # CLIENT_SECRETS, name of a file containing the OAuth 2.0 information for this
 # application, including client_id and client_secret, which are found on the 
 # API Access tab on the Google APIs Console <http://code.google.com/apis/console>
-CLIENT_SECRETS = os.path.join(os.path.dirname(__file__), 'client_secret_information.json')
+CLIENT_SECRETS = os.path.join(os.path.dirname(__file__), 'client_secrets.json')
 
 # Helpful message to display in the browser if the CLIENT_SECRETS file
 # is missing.
